cbefd5f53b4284cba2866de7e41d659fbf3bfb6b
vjpolina <voljinpolina@gmail.com>
2026-05-27 08:50:35 +0300

    A fost completat exercitiul practic pentru ziua 3

--- exersare.php
+++ exersare.php
@@ -1,5 +1,5 @@
 <?php
-echo "Hello, World!";
+echo "Hello, World!" . "<br>" . "<br>";
 echo "<script>console.log('Potatoes!');</script>";
 
 /*
@@ -16,3 +16,23 @@ echo $js_code;
 
 console_log('text here');
 */
+ $Numere = [13, 135, 31, 49, 51, 99, 74, 86, 97, 2, 89];
+
+ echo "Numerele din array sunt: ";
+ foreach ($Numere as $numar) {
+     echo $numar . " ";
+ }
+ echo "<br>";
+
+$nr_par=0;
+$nr_impare=0;
+$i=0;
+
+for($i=0; $i<10; $i++){
+    if($Numere[$i]%2==0) $nr_par++;
+    else $nr_impare++;
+}
+
+echo "În array sunt " . $nr_par . " numere pare, și " . $nr_impare . " numere impare.";
+
+
